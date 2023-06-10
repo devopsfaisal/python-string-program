c28e3fdb8b59ea81d88185704250241962682c34
devopsfaisal <clumsyfaisal@gmail.com>
2023-06-10 10:50:25 +0530

    Program to print characters at even and odd index in the string separately using slice operator

---
+++ char_at_eveodd_using_slice.py
@@ -0,0 +1,6 @@
+# Program to print characters at even and odd index in the string separately using slice operator
+
+s = input('Enter the string: ')
+
+print('The characters present at even index: ',s[::2])
+print('The characters present at even index: ',s[1::2])
